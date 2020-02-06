2823300ad3921ba2423c52c9867ba00eac7b7dd5
davidgonzalezfx <dvdramos16@gmail.com>
2020-02-06 12:52:03 -0500

    [Fix] Changed the struct

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -12,18 +12,18 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	printf("The last digit of %i is %i", n, n % 10);
+	printf("The last digit of %i is ", n);
 	if (n % 10 > 5)
 	{
-		printf(" and is greater than 5\n");
+		printf("%i and is greater than 5\n", n % 10);
 	}
 	else if (n % 10 == 0)
 	{
-		printf(" and is 0\n");
+		printf("%i and is 0\n", n % 10);
 	}
-	else if (n % 10 < 6 && n % 10 != 0)
+	else
 	{
-		printf(" and is less than 6 and not 0\n");
+		printf("%i and is less than 6 and not 0\n", n % 10);
 	}
 	return (0);
 }

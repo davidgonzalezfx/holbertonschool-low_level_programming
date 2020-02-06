6dd435cb0b52d6877ece7742353a0b57a3573a16
davidgonzalezfx <dvdramos16@gmail.com>
2020-02-06 09:37:29 -0500

    [Feature] Check some conditions with the last digit of a random number

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/* more headers goes there */
+
+/**
+ * main - main function
+ * Description: From random number evaluate conditions
+ * Return: 0
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
+	printf("The last digit of %i is %i", n n % 10);
+	if (n % 10 > 5)
+	{
+		printf(" and is greater than 5");
+	}
+	else if (n % 10 == 0)
+	{
+		printf(" and is 0");
+	}
+	else if (n % 10 < 6 && n % 10 != 0)
+	{
+		printf(" and is less than 6 and not 0");
+	}
+	return (0);
+}

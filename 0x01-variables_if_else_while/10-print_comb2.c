91adcd9ec2d7efd607025e8f06805fdb3814c2c8
davidgonzalezfx <dvdramos16@gmail.com>
2020-02-06 13:11:57 -0500

    [Fix] Change variable declarations

--- 0x01-variables_if_else_while/10-print_comb2.c
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -6,9 +6,12 @@
  */
 int main(void)
 {
-	for (int i = '0'; i <= '9'; i++)
+	int i;
+	int j;
+
+	for (i = '0'; i <= '9'; i++)
 	{
-		for (int j = '0'; j <= '9'; j++)
+		for (j = '0'; j <= '9'; j++)
 		{
 			putchar(i);
 			putchar(j);

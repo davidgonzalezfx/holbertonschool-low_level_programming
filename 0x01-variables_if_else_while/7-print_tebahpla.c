928bfd0da4c28c9924232206cb95e7e81131f5b1
davidgonzalezfx <dvdramos16@gmail.com>
2020-02-06 11:46:41 -0500

    [Feature] Prints alphabet in reverse

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ * Return: 0
+ */
+int main(void)
+{
+	char i;
+
+	for (i = 'z'; i >= 'a'; i--)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}

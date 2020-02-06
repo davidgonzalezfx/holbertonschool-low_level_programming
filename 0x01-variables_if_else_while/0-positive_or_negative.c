23e31cfa6b755b38c74d5cd267c76705d19c8e6a
davidgonzalezfx <dvdramos16@gmail.com>
2020-02-06 12:13:30 -0500

    [Fix] change variable output type in printf

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -17,15 +17,15 @@ int main(void)
 	/* your code goes there */
 	if (n > 0)
 	{
-		printf("%i is positive", n);
+		printf("%d is positive", n);
 	}
 	else if (n == 0)
 	{
-		printf("%i is zero", n);
+		printf("%d is zero", n);
 	}
 	else if (n < 0)
 	{
-		printf("%i is negative", 0);
+		printf("%d is negative", 0);
 	}
 	return (0);
 }

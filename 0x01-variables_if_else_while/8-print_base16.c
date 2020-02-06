05bd3527dbeffdec2a2981a97465013fcd723af9
davidgonzalezfx <dvdramos16@gmail.com>
2020-02-06 11:57:14 -0500

    [Feature] Print hex numbers

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ * Return: 0
+ */
+int main(void)
+{
+	char i;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		putchar(i);
+	}
+	for (i = 'A'; i <= 'F'; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}

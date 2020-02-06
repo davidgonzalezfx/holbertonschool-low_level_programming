834a7fc2b302d257c59e7a48186b05161b0a5b09
davidgonzalezfx <dvdramos16@gmail.com>
2020-02-06 12:43:08 -0500

    [Fix] Breakline added

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -15,15 +15,15 @@ int main(void)
 	printf("The last digit of %i is %i", n, n % 10);
 	if (n % 10 > 5)
 	{
-		printf(" and is greater than 5");
+		printf(" and is greater than 5\n");
 	}
 	else if (n % 10 == 0)
 	{
-		printf(" and is 0");
+		printf(" and is 0\n");
 	}
 	else if (n % 10 < 6 && n % 10 != 0)
 	{
-		printf(" and is less than 6 and not 0");
+		printf(" and is less than 6 and not 0\n");
 	}
 	return (0);
 }

3752c4670174a743cdcfe092c3f012e65cf01769
davidgonzalezfx <dvdramos16@gmail.com>
2020-02-06 12:39:45 -0500

    [Fix] Comma missed

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -12,7 +12,7 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	printf("The last digit of %i is %i", n n % 10);
+	printf("The last digit of %i is %i", n, n % 10);
 	if (n % 10 > 5)
 	{
 		printf(" and is greater than 5");

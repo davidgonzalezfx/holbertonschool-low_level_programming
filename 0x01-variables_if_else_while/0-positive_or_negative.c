c3ed89c240a0cda6fe05ee80acfca0476a29f8e3
davidgonzalezfx <dvdramos16@gmail.com>
2020-02-06 12:15:40 -0500

    [Fix] Return to int type, fix typo

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -17,15 +17,15 @@ int main(void)
 	/* your code goes there */
 	if (n > 0)
 	{
-		printf("%d is positive", n);
+		printf("%i is positive", n);
 	}
 	else if (n == 0)
 	{
-		printf("%d is zero", n);
+		printf("%i is zero", n);
 	}
 	else if (n < 0)
 	{
-		printf("%d is negative", 0);
+		printf("%i is negative", n);
 	}
 	return (0);
 }

9cf82f5ca232bd6f8900820f0773ff8073540ff3
davidgonzalezfx <dvdramos16@gmail.com>
2020-02-06 12:17:42 -0500

    [Fix] Breakline added

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -14,18 +14,17 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	/* your code goes there */
 	if (n > 0)
 	{
-		printf("%i is positive", n);
+		printf("%i is positive\n", n);
 	}
 	else if (n == 0)
 	{
-		printf("%i is zero", n);
+		printf("%i is zero\n", n);
 	}
-	else if (n < 0)
+	else (n < 0)
 	{
-		printf("%i is negative", n);
+		printf("%i is negative\n", n);
 	}
 	return (0);
 }

1d20d65cab89c82dff6f9e6a24958be03cbc9f67
davidgonzalezfx <dvdramos16@gmail.com>
2020-02-06 19:35:26 -0500

    [Feature] Special printing

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+ * main - main function
+ * Description - Print combination fo two different numbers
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+	int j;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		for (j = '1'; j <= '9'; j++)
+		{
+			if (i != j && i < j)
+			{
+				putchar(i);
+				putchar(j);
+				if (i != '8')
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	return (0);
+}

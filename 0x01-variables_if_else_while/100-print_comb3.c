ad0afe0be30588b1933baaa0a16d9d5132658c59
davidgonzalezfx <dvdramos16@gmail.com>
2020-02-06 19:38:34 -0500

    [Fix] Change some conditions

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -9,15 +9,15 @@ int main(void)
 	int i;
 	int j;
 
-	for (i = '0'; i <= '9'; i++)
+	for (i = '0'; i <= '8'; i++)
 	{
-		for (j = '1'; j <= '9'; j++)
+		for (j = i + 1; j <= '9'; j++)
 		{
-			if (i != j && i < j)
+			if (i != j)
 			{
 				putchar(i);
 				putchar(j);
-				if (i != '8')
+				if (i != '8' || j != '9')
 				{
 					putchar(',');
 					putchar(' ');

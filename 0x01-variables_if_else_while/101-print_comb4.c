5cb1a39ad158b882c446f67bf35ddbb5a81246ef
davidgonzalezfx <dvdramos16@gmail.com>
2020-02-06 19:57:40 -0500

    [Feature] Special print 3 digits numbers

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+/**
+ * main - main function
+ * Description - Print combination fo two different numbers
+ * Return: 0
+ */
+int main(void)
+{
+	int i, j, k;
+
+	for (i = '0'; i <= '7'; i++)
+	{
+		for (j = '1'; j <= '8'; j++)
+		{
+			for (k = '2'; k <= '9'; k++)
+			{
+				if (i < j && j < k)
+				{
+					putchar(i);
+					putchar(j);
+					putchar(k);
+					if (i != '7' || j != '8' || k != '9')
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}

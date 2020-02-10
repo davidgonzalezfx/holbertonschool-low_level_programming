996c61fe1516f27ab4440f65164c46db5c2cc4f5
davidgonzalezfx <dvdramos16@gmail.com>
2020-02-10 11:57:17 -0500

    [Feature #1] Print 10 times the alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "holberton.h"
+/**
+ * print_alphabet_x10 - print 10 times the alphabet
+ * Description - print 10 times the alphabet
+ * Return - void
+ */
+void print_alphabet_x10(void)
+{
+	int i;
+	char alph = 'a';
+
+	for (i = 0; i <= 10; i++)
+	{
+		for (alph = 'a'; alph <= 'z'; alph++)
+		{
+			_putchar(alph);
+		}
+		_putchar('\n');
+	}
+}

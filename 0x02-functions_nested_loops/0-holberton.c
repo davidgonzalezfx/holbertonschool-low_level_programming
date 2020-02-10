438418bf08cb0ddd4f772fe6a81c824a77c754fa
davidgonzalezfx <dvdramos16@gmail.com>
2020-02-10 08:43:17 -0500

    [Init] All files created

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "holberton.h"
+/**
+ * main - main function
+ * Description - Print combination fo two different numbers
+ * Return: 0
+ */
+int main(void)
+{
+	char myChar = 'd';
+	_putchar(myChar);	
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,3 @@
+# Learning functions and nested loops in C
+
+By @davidgonzalezfx

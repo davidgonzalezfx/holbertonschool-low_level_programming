6dfd654cf137c22082672ce2352d5f562b8a709c
davidgonzalezfx <dvdramos16@gmail.com>
2020-02-11 19:33:35 +0000

    [Fix #1] Delete de definition of n = -10

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -6,9 +6,8 @@
  */
 void print_to_98(int n)
 {
-	int i, n;
+	int i;
 
-	n = -10;
 	if (n > 98)
 	{
 		for (i = n; i >= 98; i--)
@@ -29,5 +28,5 @@ void print_to_98(int n)
 			}
 		}
 	}
-	printf("98");
+	printf("98\n");
 }

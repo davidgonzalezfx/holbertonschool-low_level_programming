7cb9d536ea19b9fdcbd9633f124fd9318ee7a455
davidgonzalezfx <dvdramos16@gmail.com>
2020-02-13 09:22:28 -0500

    [Feature #1] print 0 to 9 with _putchar

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+/**
+ * print_numbers - print from 0 to 9
+ * Return: void
+ */
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		_putchar(i + '0');
+	}
+	_putchar('\n');
+}

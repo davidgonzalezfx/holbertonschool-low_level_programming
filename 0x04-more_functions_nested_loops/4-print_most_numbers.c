53ff296c2d89e3421d941a175762de2271b9c44d
davidgonzalezfx <dvdramos16@gmail.com>
2020-02-13 09:34:00 -0500

    [Feature #1] Print 0 to 9, except 2 and 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+/**
+ * print_most_numbers - Don't print 2 & 4
+ * Return: void
+ */
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		if (i != 2 || i != 4)
+			_putchar(i + '0');
+	}
+	_putchar('\n');
+}

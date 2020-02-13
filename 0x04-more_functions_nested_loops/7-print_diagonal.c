dffbdfc8e39f5cc9ca36709f0083380a791fbd0a
davidgonzalezfx <dvdramos16@gmail.com>
2020-02-13 12:42:21 -0500

    [Fix #1] Logic changed

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -6,15 +6,19 @@
  */
 void print_diagonal(int n)
 {
-	int i;
+	int i, j;
 
-	if (n > 0)
+	if (n <= 0)
 	{
-		for (i = 0; i < n; i++)
-		{
-			putchar(' ');
-		}
-		putchar('\\');
+		_putchar('\n');
 	}
-	putchar('\n');
+	for (i = 0; i < n; i++)
+	{
+		for (j = 0; j < i; j++)
+		{
+			_putchar(' ');
+		}
+	_putchar('\\');
+	_putchar('\n');
+	}
 }

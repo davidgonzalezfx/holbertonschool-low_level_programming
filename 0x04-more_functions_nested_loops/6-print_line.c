1bc3a8829e14b4a670c8707abb00691847213692
davidgonzalezfx <dvdramos16@gmail.com>
2020-02-13 09:40:10 -0500

    [Feature #1] Print _ char n times

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+ * print_line - print _ character n times
+ * @n: int
+ * Return: void
+ */
+void print_line(int n)
+{
+	int i;
+
+	if (n > 0)
+	{
+		for (i = 0; i < n; i++)
+		{
+			_putchar('_');
+		}
+	}
+	_putchar('\n');
+}

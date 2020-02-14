543ae0bbe13ef49f16e9413902f56d91d0520a0d
davidgonzalezfx <dvdramos16@gmail.com>
2020-02-13 21:40:30 -0500

    [Fix #1] Change function name & use _putchar

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,19 +1,19 @@
 #include "holberton.h"
 /**
- * print_num - print any number recursivly
+ * print_number - print any number recursivly
  * @n: int
  * Return: void
  */
-void print_num(int n)
+void print_number(int n)
 {
 	if (n < 0)
 	{
-		putchar('-');
+		_putchar('-');
 		n *= -1;
 	}
 	if (n / 10 > 0)
 	{
-		print_num(n / 10);
+		print_number(n / 10);
 	}
-	putchar((n % 10) + '0');
+	_putchar((n % 10) + '0');
 }

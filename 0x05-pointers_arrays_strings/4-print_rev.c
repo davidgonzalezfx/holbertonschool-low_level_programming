0689816f007a73723c0775a36b8df874f811bdd9
davidgonzalezfx <dvdramos16@gmail.com>
2020-02-17 15:06:11 -0500

    [Feature #1] Print string in reverse order

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,17 @@
+/**
+ * print_rev - print str in reverse
+ * @s: char pointer
+ * Return: void
+ */
+void print_rev(char *s)
+{
+	int cnt, cnt2;
+
+	for (cnt = 0; *(s + cnt) != '\0'; cnt++)
+	{}
+
+	for (cnt2 = cnt - 1; cnt2 >= 0; cnt2--)
+	{
+		_putchar(*(s + cnt2));
+	}
+}

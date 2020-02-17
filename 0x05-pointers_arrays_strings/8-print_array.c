bc332d1b4d0dfabd9d2845a379e0c48cdefe1437
davidgonzalezfx <dvdramos16@gmail.com>
2020-02-17 15:07:35 -0500

    [Feature #1] Print n elements of array

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,30 @@
+/**
+ * print_nums - print n num recursivly
+ * @n: int
+ * Return: void
+ */
+void print_nums(int n)
+{
+	if (n >= 10)
+	{
+		print_nums(n / 10);
+	}
+	putchar((n % 10) + '0');
+}
+/**
+ * print_array - print n elements of a
+ * @a: int pointer
+ * @n: int
+ * Return: void
+ */
+void print_array(int *a, int n)
+{
+	int el;
+
+	for (el = 0; el < n && *(a + el) != '\0'; el++)
+	{
+		print_nums(*(a + el));
+		putchar(' ');
+	}
+	putchar('\n');
+}

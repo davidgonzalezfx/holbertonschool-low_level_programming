0deb0a99976ec4cdc6b1d8cdb702d048dc1a010c
davidgonzalezfx <dvdramos16@gmail.com>
2020-02-17 15:06:46 -0500

    [Feature #1] Prints second half of string

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,17 @@
+/**
+ * puts2 - show odd positions
+ * @str: char pointer
+ * Return: void
+ */
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0; *(str + i) != '\0'; i++)
+	{
+		if (i % 2 == 0)
+		{
+			putchar(*(str + i));
+		}
+	}
+}

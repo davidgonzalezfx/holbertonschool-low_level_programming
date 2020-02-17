e7c4b02efbb164d135cce0f1923f719ee5c26e81
davidgonzalezfx <dvdramos16@gmail.com>
2020-02-17 15:06:19 -0500

    [Feature #1] Reverse a string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * rev_string - rev a string
+ * @s: char pointer
+ * Return: void
+ */
+void rev_string(char *s)
+{
+	int init, end;
+	char aux;
+
+	for (end = 0; *(s + end) != '\0'; end++)
+	{}
+	end--;
+	for (init = 0; init != end; init++, end--)
+	{
+		aux = *(s + init);
+		*(s + init) = *(s + end);
+		*(s + end) = aux;
+	}
+}

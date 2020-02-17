e8a91decab4b39b7daa9690441c2cb2a522375f1
davidgonzalezfx <dvdramos16@gmail.com>
2020-02-17 15:07:18 -0500

    [Feature #1] Prints second half of string

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,16 @@
+/**
+ * puts_half - show second half of str
+ * @str: char pointer
+ * Return: void
+ */
+void puts_half(char *str)
+{
+	int size, half;
+
+	for (size = 0; *(str + size) != '\0'; size++)
+	{}
+	for (half = (size - 1) / 2; half != size; half++)
+	{
+		putchar(*(str + half));
+	}
+}

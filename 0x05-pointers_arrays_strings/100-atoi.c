d4f9b474ee0326dd5ee9fbea09fdf491936294a6
davidgonzalezfx <dvdramos16@gmail.com>
2020-02-18 09:52:25 -0500

    [Fix #3] Sign rule modified

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -11,21 +11,9 @@ int _atoi(char *s)
 	cnt = 0;
 	for (size = 0; *(s + size) != '\0'; size++)
 	{
-		if (*(s + size) == '-' && aux == 0)
+		if (*(s + size) == '-')
 		{
-			aux = 1;
-		}
-		else if (*(s + size) == '-' && aux == 1)
-		{
-			aux = 0;
-		}
-		if (*(s + size) == '+' && aux == 1)
-		{
-			aux = 1;
-		}
-		else if (*(s + size) == '+' && aux == 0)
-		{
-			aux = 0;
+			aux *= -1;
 		}
 		if (*(s + size) >= '0' && *(s + size) <= '9')
 		{
@@ -34,9 +22,5 @@ int _atoi(char *s)
 				break;
 		}
 	}
-	if (cnt == 0)
-		return (0);
-	if (aux == 0)
-		return (cnt *= -1);
-	return (cnt);
+	return (cnt * aux);
 }

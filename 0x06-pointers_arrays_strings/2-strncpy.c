2507fd349fe67b675ccd842a5d560ac1ea2b5d61
davidgonzalezfx <dvdramos16@gmail.com>
2020-02-20 10:11:47 -0500

    [Feature #1]  function that copies a string.

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,11 +1,18 @@
-#include "holberton.h"
-
 /**
- * _strncpy -
- *
- * Return: 
+ * _strncpy - copy two strings
+ * @dest: string
+ * @src: string
+ * @n: int
+ * Return: dest after overwrited with src
  */
 char *_strncpy(char *dest, char *src, int n)
 {
+	int i;
 
+	for (i = 0; i < n && *(src + i) != '\0'; i++)
+		*(dest + i) = *(src + i);
+	for (; i < n; i++)
+		*(dest + i) = '\0';
+
+	return (dest);
 }

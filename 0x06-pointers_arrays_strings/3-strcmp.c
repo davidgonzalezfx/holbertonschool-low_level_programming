08de1f65d4a5e5b8489cfc797e5213fce624209b
davidgonzalezfx <dvdramos16@gmail.com>
2020-02-20 12:23:36 -0500

    [Fix #1] Condition changed

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -14,7 +14,7 @@ int _strcmp(char *s1, char *s2)
 	{}
 	size1--;
 	size2--;
-	for (i = 0; *(s1 + i) != '\0' || *(s2 + i) != '\0'; i++)
+	for (i = 0; *(s1 + i) != '\0' && *(s2 + i) != '\0'; i++)
 	{
 		if (s1[i] > s2[i])
 		{

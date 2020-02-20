b7e91c443d9d86f1417327960bd8365803b14467
davidgonzalezfx <dvdramos16@gmail.com>
2020-02-20 11:40:36 -0500

    [Feature #1]  function that changes all lowercase letters of a string to uppercase

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,11 +1,17 @@
-#include "holberton.h"
-
 /**
  * reverse_array -
  *
- * Return: 
+ * Return:
  */
 void reverse_array(int *a, int n)
 {
+	int i, aux;
 
+	n--;
+	for (i = 0; i < n; i++, n--)
+	{
+		aux = a[i];
+		a[i] = a[n];
+		a[n] = aux;
+	}
 }

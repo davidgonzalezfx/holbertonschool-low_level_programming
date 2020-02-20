aef78660d177d1c79ad554534a953a0f18b6cac4
davidgonzalezfx <dvdramos16@gmail.com>
2020-02-20 10:11:28 -0500

    [Feature #1]  function that concatenates two strings.

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,11 +1,21 @@
-#include "holberton.h"
-
 /**
- * _strncat -
- *
- * Return: 
+ * _strncat - copy n chars
+ * @dest: dest string
+ * @src: src string
+ * @n: number of chars to copy
+ * Return: dest 'till n
  */
 char *_strncat(char *dest, char *src, int n)
 {
+	int i, cnt;
 
+	cnt = 0;
+	for (i = 0; dest[i] != '\0'; i++)
+	{}
+
+	for (i = i; *src && cnt < n; i++, cnt++)
+	{
+		*(dest + i) = *src++;
+	}
+	return (dest);
 }

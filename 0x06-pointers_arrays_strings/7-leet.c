d0ad45c2d2bd46d3a1c06ea2b4bf0dd4428e9c8e
davidgonzalezfx <dvdramos16@gmail.com>
2020-02-20 13:20:27 -0500

    [Feature #1] encodes a string into 1337

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,11 +1,35 @@
-#include "holberton.h"
-
 /**
- * leet -
- *
- * Return: 
+ * leet - encode a string into 1337
+ * @s: string
+ * Return: s encoded
  */
-char *leet(char *)
+char *leet(char *s)
 {
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] == 'a' || s[i] == 'A')
+		{
+			s[i] = '4';
+		}
+		else if (s[i] == 'e' || s[i] == 'E')
+		{
+			s[i] = '3';
+		}
+		else if (s[i] == 'o' || s[i] == 'O')
+		{
+			s[i] = '0';
+		}
+		else if (s[i] == 't' || s[i] == 'T')
+		{
+			s[i] = '7';
+		}
+		else if (s[i] == 'l' || s[i] == 'L')
+		{
+			s[i] = '1';
+		}
+	}
+	return (s);
 
 }

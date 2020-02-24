f058a0f8d90fa28a5bb183027c0088f96f4c2ada
davidgonzalezfx <dvdramos16@gmail.com>
2020-02-24 09:12:59 -0500

    [Feature #1] copies memory area

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,11 +1,19 @@
 #include "holberton.h"
 
 /**
- * _memcpy -
- *
- * Return: 
+ * _memcpy - copies memory area
+ * @dest: char pointer
+ * @src: char pointer
+ * @n: int limit
+ * Return: dest copied from src
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
+	unsigned int limit;
 
+	for (limit = 0; limit < n; limit++)
+	{
+		*(dest + limit) = *(src + limit);
+	}
+	return (dest);
 }

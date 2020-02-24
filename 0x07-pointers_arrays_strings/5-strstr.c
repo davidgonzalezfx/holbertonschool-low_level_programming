93f0f7536c3a80e1d752a31bbdc97a2c74cf2aa9
davidgonzalezfx <dvdramos16@gmail.com>
2020-02-24 10:45:39 -0500

    [Feature #1] locates a substring

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,11 +1,26 @@
 #include "holberton.h"
 
 /**
- * _strstr -
- *
- * Return: 
+ * _strstr -  locates a substring
+ * @haystack: char pointer
+ * @needle: char pointer
+ * Return: char pointer
  */
 char *_strstr(char *haystack, char *needle)
 {
+	int i, j;
 
+	for (i = 0; haystack[i] != '\0'; i++)
+	{
+		for (j = 0; needle[j] != '\0'; j++)
+		{
+			if (needle[j] != haystack[i])
+			{
+				haystack++;
+				break;
+			}
+			continue;		
+		}
+	}
+	return (haystack);
 }

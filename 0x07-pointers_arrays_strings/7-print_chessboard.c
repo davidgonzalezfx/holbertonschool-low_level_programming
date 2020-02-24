35569e1e18dd3b3818a0d64f0917fde8ed4c0f21
davidgonzalezfx <dvdramos16@gmail.com>
2020-02-24 13:21:09 -0500

    [Feature #1] Print chessboard

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,11 +1,20 @@
 #include "holberton.h"
 
 /**
- * print_chessboard -
- *
- * Return: 
+ * print_chessboard - print chessboard
+ * @a: matrix
+ * Return: void
  */
 void print_chessboard(char (*a)[8])
 {
+	int col, row;
 
+	for (row = 0; row < 8; row++)
+	{
+		for (col = 0; col < 8; col++)
+		{
+			_putchar(a[row][col]);
+		}
+		_putchar('\n');
+	}
 }

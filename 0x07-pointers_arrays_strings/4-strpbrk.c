0241571d3e6997016d1c851e68847afd66c71270
davidgonzalezfx <dvdramos16@gmail.com>
2020-02-24 10:24:31 -0500

    [Feature #1] searches a string for any of a set of bytes

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -7,5 +7,20 @@
  */
 char *_strpbrk(char *s, char *accept)
 {
+	int i, j, limit;
 
+	limit = 1;
+	for (i = 0; limit != 0; i++)
+	{
+		s++;
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (s[i] == accept[j])
+			{	
+				limit = 0;
+				break;
+			}
+		}
+	}
+	return (s);
 }

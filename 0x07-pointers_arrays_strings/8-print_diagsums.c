8d156b4a44241db5f9987f39ed283233aab05617
davidgonzalezfx <dvdramos16@gmail.com>
2020-02-24 14:25:02 -0500

    [Feature #1] prints the sum of the two diagonals

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -1,11 +1,26 @@
 #include "holberton.h"
+#include <stdio.h>
 
 /**
- * print_diagsums -
- *
- * Return: 
+ * print_diagsums - prints the sum of the two diagonals
+ * @a: matris -> array
+ * @size: size
+ * Return: prints
  */
 void print_diagsums(int *a, int size)
 {
+	int first = 0, second = 0, i, j, aux = 0;
 
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < size; j++)
+		{
+			if (i == j)
+				first += *(a + aux);
+			if (i + j + 1 == size)
+				second += *(a + aux);
+			aux++;
+		}
+	}
+	printf("%u, %u\n", first, second);
 }

6f5fa495a19523ed8469f50895a9fac4ba4fae6b
davidgonzalezfx <dvdramos16@gmail.com>
2020-02-26 08:16:57 -0500

    [Feature #1] factorial of given number

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -1,11 +1,17 @@
 #include "holberton.h"
 
 /**
- * factorial -
- *
- * Return: 
+ * factorial - factorial of a given number
+ * @n: int
+ * Return: int
  */
 int factorial(int n)
 {
-
+	if (n == 0 || n == 1)
+		return (1);
+	if (n > 0)
+		return (n * factorial(n - 1));
+	if (n < 0)
+		return (-1);
+	return (1);
 }

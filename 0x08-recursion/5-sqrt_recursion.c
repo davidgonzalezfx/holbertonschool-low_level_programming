035f2826482f3b961b55cd586952feb56806c307
davidgonzalezfx <dvdramos16@gmail.com>
2020-02-26 09:19:24 -0500

    [Feature #1] natural square root of a number

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,11 +1,28 @@
 #include "holberton.h"
 
 /**
- * _sqrt_recursion -
- *
- * Return: 
+ * init_sqrt - natural square root of a number, from 1
+ * @n: int
+ * Return: sqrt(n) 
+ */
+int init_sqrt(int i, int n)
+{
+	if (n == 0)
+		return (0);
+	if (i * i == n)
+		return (i);
+	if (i * i > n)
+		return (-1);
+	else
+		return (init_sqrt(i+1, n));
+}
+
+/**
+ * _sqrt_recursion - natural square root of a number
+ * @n: int
+ * Return: sqrt(n) 
  */
 int _sqrt_recursion(int n)
 {
-
+	return (init_sqrt(1, n));
 }

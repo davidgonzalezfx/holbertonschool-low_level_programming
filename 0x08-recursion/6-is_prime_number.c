72e39bd80644f97bbf9c133a6df6340840bf7390
davidgonzalezfx <dvdramos16@gmail.com>
2020-02-26 09:38:39 -0500

    [Feature #1] Check prime numbers: 1 if yes, 0 if not

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,11 +1,29 @@
 #include "holberton.h"
 
 /**
- * is_prime_number -
- *
- * Return: 
+ * init_prime - check prime  from 2
+ * @cnt: int
+ * @n: int
+ * Return: int
+ */
+int init_prime(int cnt, int n)
+{
+	if (n <= 1)
+		return 0;
+	if (n == cnt)
+		return (1);
+	if (n % cnt == 0)
+		return 0;
+	return init_prime(++cnt, n);
+}
+
+
+/**
+ * is_prime_number - 1 if prime 0 if not
+ * @n: int
+ * Return: int
  */
 int is_prime_number(int n)
 {
-
+	return (init_prime(2, n));
 }

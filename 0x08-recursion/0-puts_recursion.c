076cbd5da3211f5fa7d802f69d2a0b16c3348c46
davidgonzalezfx <dvdramos16@gmail.com>
2020-02-26 08:03:57 -0500

    [Feature #1] print string recursively

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,11 +1,19 @@
 #include "holberton.h"
 
 /**
- * _puts_recursion -
- *
- * Return: 
+ * _puts_recursion - print string recursively
+ * @s: char
+ * Return: voide
  */
 void _puts_recursion(char *s)
 {
-
+	if (*s)
+	{
+		_putchar(*s);
+		_puts_recursion(++s);
+	}
+	else
+	{
+		_putchar('\n');
+	}
 }

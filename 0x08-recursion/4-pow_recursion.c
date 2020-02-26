9756e594f269f9b17bbad59bcb4cfa4ec451a0dd
davidgonzalezfx <dvdramos16@gmail.com>
2020-02-26 08:19:36 -0500

    [Feature #1] x raised to the power of y

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,11 +1,16 @@
 #include "holberton.h"
 
 /**
- * _pow_recursion -
- *
- * Return: 
+ * _pow_recursion - x raised to the power of y
+ * @x: base
+ * @y: power
+ * Return: int x ** y
  */
 int _pow_recursion(int x, int y)
 {
-
+	if (y == 0)
+		return (1);
+	if (y < 0)
+		return (-1);
+	return (x * _pow_recursion(x, y - 1));
 }

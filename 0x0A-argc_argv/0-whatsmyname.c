cc2f343285335ccfaa3bb4636a796f6716a36961
davidgonzalezfx <dvdramos16@gmail.com>
2020-02-28 11:51:11 -0500

    [Feature #1] Program that prints its name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+/**
+ * main - main function
+ * @argc: arguments count
+ * @argv: arguments array
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}

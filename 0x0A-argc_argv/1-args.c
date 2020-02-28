e35ffcdb3d6c3df5c11b92d3d3c0a85b7d58a95a
davidgonzalezfx <dvdramos16@gmail.com>
2020-02-28 12:06:41 -0500

    [Feature #1] prints the number of arguments

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+/**
+ * main - main function
+ * @argc: arguments count
+ * @argv: arguments array
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	printf("%d", --argc);
+	return (0);
+}

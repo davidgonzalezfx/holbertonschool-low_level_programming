901af4c006dd145c2a90e2ca543ce7d1af830131
davidgonzalezfx <dvdramos16@gmail.com>
2020-02-29 14:50:07 -0500

    [Fix #1] Compile with unused variable

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,11 +1,11 @@
-#include "holberton.h"
+#include <stdio.h>
 /**
  * main - main function
  * @argc: arguments count
  * @argv: arguments array
  * Return: 0
  */
-int main(int argc, char *argv[])
+int main(__attribute__((unused)) int argc, char *argv[])
 {
 	printf("%s\n", argv[0]);
 	return (0);

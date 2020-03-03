1c621b76499def36f330f18a8db6259b098aa55e
DavidGonzalezFx <dvdramos16@gmail.com>
2020-03-03 12:29:08 -0500

    [Fix #2] Return NULL if fails

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -12,10 +12,10 @@ char *create_array(unsigned int size, char c)
 	unsigned int i;
 
 	if (size == 0)
-	{
-		return NULL;
-	}
+		return (NULL);
 	p = malloc(sizeof(char) * size);
+	if (!p)
+		return (NULL);
 	for (i = 0; i < size; i++)
 	{
 		*(p + i) = c;

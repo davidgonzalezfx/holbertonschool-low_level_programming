a4fa9cbd7a5661fec5051a09828c965dc6fead94
DavidGonzalezFx <dvdramos16@gmail.com>
2020-03-03 13:28:06 -0500

    [Feature #1] New pointer with copy of other

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,11 +1,36 @@
+
 #include "holberton.h"
+/**
+ * _strlen - return length
+ * @s: char
+ * Return: int
+ */
+int _strlen(char *s)
+{
+	if (*s)
+		return (1 + _strlen(s + 1));
+	else
+		return (0);
+}
 
 /**
- * _strdup -
- *
- * Return: 
+ * _strdup - new pointer with copy of other
+ * @str: char pointer
+ * Return: char pointer
  */
 char *_strdup(char *str)
 {
+	char *newCopy, *start;
+	int size = _strlen(str);
 
+	newCopy = malloc(sizeof(char) * size);
+	start = newCopy;
+	if (newCopy)
+	{
+		for (; *str;)
+			*newCopy++ = *str++;
+		newCopy = start;
+		return (newCopy);
+	}
+	return (NULL);
 }

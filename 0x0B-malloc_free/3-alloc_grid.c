d742077d65bee3c658ff284af59501048b76de69
davidgonzalezfx <dvdramos16@gmail.com>
2020-03-03 16:49:05 -0500

    [Feature #1] Create matrix with double pointer

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -1,11 +1,28 @@
 #include "holberton.h"
 
 /**
- * alloc_grid -
- *
- * Return: 
+ * alloc_grid - allocate a matrix
+ * @width: width
+ * @height: height
+ * Return: matrix allocated
  */
 int **alloc_grid(int width, int height)
 {
+	int col, row, **matrix = NULL;
 
+	if (width == 0 || height == 0)
+		return (NULL);
+	matrix = malloc(sizeof(int *) * height);
+	for (row = 0; row < height; row++)
+	{
+		matrix[row] = malloc(sizeof(int) * width);
+	}
+	for (row = 0; row < width; row++)
+	{
+		for (col = 0; col < height; col++)
+		{
+			*(matrix)[col] = 0;
+		}
+	}
+	return (matrix);
 }

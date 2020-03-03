bbf89584e5a2f5fb66a0273e765b127fc44c49f2
davidgonzalezfx <dvdramos16@gmail.com>
2020-03-03 18:06:08 -0500

    [Fix #3] Free memory

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -8,7 +8,7 @@
  */
 int **alloc_grid(int width, int height)
 {
-	int col, row, **matrix = NULL;
+	int col, row, **matrix = NULL, i;
 
 	if (width <= 0 || height <= 0)
 		return (NULL);
@@ -19,7 +19,12 @@ int **alloc_grid(int width, int height)
 	{
 		matrix[row] = malloc(sizeof(int) * width);
 		if (!matrix[row])
+		{
+			for (i = 0; i < row; i++)
+				free(matrix[i]);
+			free(matrix);
 			return (NULL);
+		}
 	}
 	for (row = 0; row < width; row++)
 	{

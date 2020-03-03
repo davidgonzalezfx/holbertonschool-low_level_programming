0ca0bc48f77d19938aa70701b7286247d3cee449
davidgonzalezfx <dvdramos16@gmail.com>
2020-03-03 15:38:49 -0500

    [Fix #1] NULL = empty string, null at then end of new

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -12,8 +12,10 @@ char *str_concat(char *s1, char *s2)
 	int size1, size2, i, j;
 
 
-	if (s1 == NULL || s2 == NULL)
-		return (NULL);
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
 	for (size1 = 0; s1[size1] != '\0'; size1++)
 	;
 	for (size2 = 0; s2[size2] != '\0'; size2++)
@@ -23,6 +25,6 @@ char *str_concat(char *s1, char *s2)
 		*(new + i) = s1[i];
 	for (i = size1, j = 0; j < size2; i++, j++)
 		*(new + i) = s2[j];
-
+	*(new + i) = '\0';
 	return (new);
 }

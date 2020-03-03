02eaeeda4d1964721f772d8645aa0d5dc71e0b7d
DavidGonzalezFx <dvdramos16@gmail.com>
2020-03-03 12:19:23 -0500

    [Feature #1] creates an array of chars, and initializes it with a specific char.

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -11,7 +11,7 @@ char *create_array(unsigned int size, char c)
 	char *p;
 	unsigned int i;
 
-	if ( size == 0)
+	if (size == 0)
 	{
 		return 0;
 	}
@@ -20,5 +20,5 @@ char *create_array(unsigned int size, char c)
 	{
 		*(p + i) = c;
 	}
-	return p;
+	return (p);
 }

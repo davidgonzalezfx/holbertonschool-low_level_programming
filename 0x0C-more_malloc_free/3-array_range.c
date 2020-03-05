44482ca32ea1966d5776c50137f8c3e5c65ea32f
davidgonzalezfx <dvdramos16@gmail.com>
2020-03-05 15:53:57 -0500

    [Feature #1] Create array of int from min, to max

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -1,11 +1,21 @@
 #include "holberton.h"
 
 /**
- * array_range -
- *
- * Return: 
+ * array_range - create array of int
+ * @min: start
+ * @max: end
+ * Return: int pointer
  */
 int *array_range(int min, int max)
 {
+	int *arr = NULL, i;
 
+	if (min > max)
+		return (NULL);
+	arr = malloc(max - min);
+	if (!arr)
+		return (NULL);
+	for (i = 0; i <= max - min; i++)
+		*(arr + i) = min + i;
+	return (arr);
 }

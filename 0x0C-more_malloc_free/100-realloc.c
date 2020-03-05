081fc05ad256de3617c3aa681df23f59932c2c62
davidgonzalezfx <dvdramos16@gmail.com>
2020-03-05 18:34:52 -0500

    [Fix #5] Order changed

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -14,15 +14,12 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		return (ptr);
 	if (ptr == NULL)
 		ptr = malloc(new_size);
-	if (new_size > old_size)
-	{
-		free(ptr);
-		ptr = malloc(new_size);
-	}
 	if (new_size == 0 && ptr != NULL)
 	{
 		free(ptr);
 		return (NULL);
 	}
+	free(ptr);
+	ptr = malloc(new_size);
 	return (ptr);
 }

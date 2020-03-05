f8dc42a230f557a72923094ea7a4efd0bab6c70d
davidgonzalezfx <dvdramos16@gmail.com>
2020-03-05 18:31:06 -0500

    [Fix #3] ptr = new_size if new_size > old_size

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -16,7 +16,11 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	if (ptr == NULL)
 		new = malloc(new_size);
 	if (new_size > old_size)
-		new = malloc(new_size);
+	{
+		free(ptr);
+		ptr = malloc(new_size);
+		return (ptr);
+	}
 	if (new_size == 0 && ptr != NULL)
 	{
 		free(ptr);

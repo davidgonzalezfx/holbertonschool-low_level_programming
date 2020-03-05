f5433716e5272f79bd9d88b204ceb1178f5d9154
davidgonzalezfx <dvdramos16@gmail.com>
2020-03-05 18:43:27 -0500

    [Fix #7] If's exclusive

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -12,10 +12,9 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 
 	if (new_size == old_size)
 		return (ptr);
-	if (ptr == NULL)
+	else if (ptr == NULL)
 		ptr = malloc(new_size);
-		return (ptr);
-	if (new_size == 0 && ptr != NULL)
+	else if (new_size == 0 && ptr != NULL)
 	{
 		free(ptr);
 		return (NULL);

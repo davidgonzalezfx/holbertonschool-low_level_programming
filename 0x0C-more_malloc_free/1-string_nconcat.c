55007799d67e1a0108aab057b1111321bb7a6445
davidgonzalezfx <dvdramos16@gmail.com>
2020-03-05 12:05:02 -0500

    [Feature #1] concatenates two strings

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,4 +1,5 @@
 #include "holberton.h"
+#include <stdio.h>
 
 /**
  * string_nconcat -
@@ -7,5 +8,19 @@
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
+	char *newConcat = NULL;
+	unsigned int size1 = 0, size2 = 0, index = 0, limit = 0;
 
+	for (; s1[size1] != '\0'; size1++)
+	{};
+	for (; s2[size2] != '\0'; size2++)
+	{};
+	newConcat = malloc(size1 + size2);
+	if (!newConcat)
+		return (NULL);
+	for(; *s1; index++, s1++)
+		*(newConcat + index) = *s1;
+	for (; limit <= n; index++, s2++, limit++)
+		*(newConcat + index) = *s2;
+	return (newConcat);
 }

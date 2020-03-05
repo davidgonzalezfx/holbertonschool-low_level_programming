a6bdfe22a8321f8711ead39c5dfc4531d548b6e6
davidgonzalezfx <dvdramos16@gmail.com>
2020-03-05 16:08:20 -0500

    [Fix #2] sizeof() *

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -12,7 +12,7 @@ int *array_range(int min, int max)
 
 	if (min > max)
 		return (NULL);
-	arr = malloc(sizeof(int) + (max - min + 1));
+	arr = malloc(sizeof(int) * (max - min + 1));
 	if (!arr)
 		return (NULL);
 	for (i = 0; i <= max - min; i++)

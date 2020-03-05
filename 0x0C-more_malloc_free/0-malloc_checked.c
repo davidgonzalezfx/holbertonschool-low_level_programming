bc86bcb4561f4ea1ffbd738094f76e9d0e709553
davidgonzalezfx <dvdramos16@gmail.com>
2020-03-05 11:39:42 -0500

    [Feature #1]  allocates memory using malloc

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,11 +1,16 @@
 #include "holberton.h"
 
 /**
- * malloc_checked -
- *
- * Return: 
+ * malloc_checked - allocates memory using malloc
+ * @b: unsigned int
+ * Return: void pointer
  */
 void *malloc_checked(unsigned int b)
 {
+	void *p;
 
+	p = malloc(b);
+	if (!p)
+		exit(98);
+	return (p);
 }

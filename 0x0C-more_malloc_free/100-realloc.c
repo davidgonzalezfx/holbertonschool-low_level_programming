d7f098f37b5e5f9c251b74dae58211de989131c0
davidgonzalezfx <dvdramos16@gmail.com>
2020-03-05 18:41:05 -0500

    [Fix #6] return ptr missing

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -10,10 +10,11 @@
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
 
-	if (new_size == old_size)
+	if (new_size == old_size)	
 		return (ptr);
 	if (ptr == NULL)
 		ptr = malloc(new_size);
+		return (ptr);
 	if (new_size == 0 && ptr != NULL)
 	{
 		free(ptr);

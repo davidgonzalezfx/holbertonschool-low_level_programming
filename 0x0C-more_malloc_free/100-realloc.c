6caae93b51b03ac92693177b1c01b067d4f1afb9
davidgonzalezfx <dvdramos16@gmail.com>
2020-03-05 18:20:32 -0500

    [Fix #1] size == 0 && ptr != NULL

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -17,6 +17,11 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		new = malloc(new_size);
 	if (new_size > old_size)
 		new = malloc(new_size);
+	if (new_size == 0 && ptr != NULL)
+	{
+		free(ptr);
+		return (NULL);
+	}
 
 	return (new);
 }

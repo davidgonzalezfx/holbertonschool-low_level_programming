8f98403d02af2f2e359476d2a0550d3b49486cba
davidgonzalezfx <dvdramos16@gmail.com>
2020-03-05 18:18:28 -0500

    [Featur #1] realloc

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -1,11 +1,22 @@
 #include "holberton.h"
 
 /**
- * _realloc -
- *
- * Return: 
+ * _realloc - reallocates memory
+ * @ptr: prev pointer
+ * @old_size: prev size
+ * @new_size: new size
+ * Return: void pointer
  */
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
+	char *new = NULL;
 
+	if (new_size == old_size)
+		return (ptr);
+	if (ptr == NULL)
+		new = malloc(new_size);
+	if (new_size > old_size)
+		new = malloc(new_size);
+
+	return (new);
 }

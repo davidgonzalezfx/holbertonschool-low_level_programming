c4584de5e27fbba702255ddaea1eb5b322e8ebe4
davidgonzalezfx <dvdramos16@gmail.com>
2020-03-09 12:06:51 -0500

    [Feature] dog_t alias struct

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -9,17 +9,16 @@
  *
  * Description: define dog datatype
  */
-typedef struct dog {
+struct dog {
 	char *name;
 	float age;
 	char *owner;
-} dog;
+};
 
-typedef dog dog_t;
-
+typedef struct dog dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
-
+void free_dog(dog_t *d);
 
 #endif

1cb623a433876754b6a4c59cf036133b12eb7d6e
davidgonzalezfx <dvdramos16@gmail.com>
2020-03-09 14:11:25 -0500

    [Fix] Betty linter

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -46,10 +46,8 @@ dog_t *new_dog(char *name, float age, char *owner)
 		free(newDog);
 		return (NULL);
 	}
-
 	newDog->name = cpname;
 	newDog->age = age;
 	newDog->owner = cpowner;
-
 	return (newDog);
 }

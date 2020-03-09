09e951b712662b22c414bcf7ccdd3e4b7fe3f6a9
davidgonzalezfx <dvdramos16@gmail.com>
2020-03-09 12:06:25 -0500

    [Feature #1] Print dog data

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -1,11 +1,22 @@
 #include "dog.h"
+#include <stdio.h>
 
 /**
- * new_dog -
- *
- * Return: 
+ * print_dog - print dog struct
+ * @d: dog *
+ * Return: void (just prints)
  */
-dog_t *new_dog(char *name, float age, char *owner)
+void print_dog(struct dog *d)
 {
-
+	if(!d)
+		return;
+	if (d->name == NULL)
+		printf("Name: (nil)\n");
+	else
+		printf("Name: %s\n", d->name);
+	printf("Age: %f\n", d->age);
+	if (d->owner == NULL)
+		printf("Owner: (nil)\n");
+	else
+		printf("Owner: %s\n", d->owner);
 }

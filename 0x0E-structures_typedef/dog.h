7388546d9bd4cbc15d3440cbc520805266b09285
davidgonzalezfx <dvdramos16@gmail.com>
2020-03-09 10:28:09 -0500

    [Library] All functions added

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -9,10 +9,17 @@
  *
  * Description: define dog datatype
  */
-struct dog {
+typedef struct dog {
 	char *name;
 	float age;
 	char *owner;
-};
+} dog;
+
+typedef dog dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+
 
 #endif

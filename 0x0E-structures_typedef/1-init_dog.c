40645bd6500d5b0e8d573de165196253f09819f0
davidgonzalezfx <dvdramos16@gmail.com>
2020-03-09 13:36:58 -0500

    [Fix] Betty linter

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -3,11 +3,10 @@
 
 /**
  * init_dog - initialize dog
- * @dog: struct dog
+ * @d: struct dog
  * @name: char *
  * @age: float
  * @owner: char *
- * Return: void
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {

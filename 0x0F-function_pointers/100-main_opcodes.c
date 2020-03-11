59b3b73e243302754f6cb77eed2fd1dc58bedbb0
davidgonzalezfx <dvdramos16@gmail.com>
2020-03-11 15:58:08 -0500

    [Feature #1] prints opcode

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,36 @@
+#include "function_pointers.h"
+#include <stdio.h>
+
+/**
+ * main - main
+ * @ac: argument count
+ * @av: argument array
+ * Return: int
+ */
+int main(int ac, char *av[])
+{
+	int *(fun)(int, char **);
+	unsigned char *ptr;
+	int param = 0, i = 0;
+
+	if (ac != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+	param = atoi(av[1]);
+	if (param < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+	ptr = (unsigned char *)main;
+	for (; i < param; i++)
+	{
+		printf("%.2x", ptr[i]);
+		if (i < param)
+			printf(" ");
+	}
+	printf("\n");
+	return (0);
+}

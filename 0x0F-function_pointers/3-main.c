9eeacee53ee9f2078043c4e82662b3b598fcc56c
davidgonzalezfx <dvdramos16@gmail.com>
2020-03-11 13:35:29 -0500

    [Fix] '\0' char

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -21,7 +21,7 @@ int main(int argc, char *argv[])
 	    argv[2][0] != '-' &&
 	    argv[2][0] != '*' &&
 	    argv[2][0] != '/' &&
-	    argv[2][0] != '%') || argv[2][0] == NULL)
+	    argv[2][0] != '%') || argv[2][1] != '\0')
 	{
 		printf("Error\n");
 		exit(99);

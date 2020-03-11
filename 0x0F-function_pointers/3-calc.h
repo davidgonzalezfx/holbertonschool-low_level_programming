a5031bd027998a09cc397836fe602b7cad3966a8
davidgonzalezfx <dvdramos16@gmail.com>
2020-03-11 13:23:49 -0500

    [Feature #1] Struct op added

---
+++ 0x0F-function_pointers/3-calc.h
@@ -0,0 +1,14 @@
+#ifndef CALC_H
+#define CALC_H
+/**
+ * struct op - Struct op
+ *
+ * @op: The operator
+ * @f: The function associated
+ */
+typedef struct op
+{
+	char *op;
+	int (*f)(int a, int b);
+} op_t;
+#endif

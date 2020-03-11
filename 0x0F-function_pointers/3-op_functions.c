f4fd10b42154f3eae8870c0f1bf13a623091a1e0
davidgonzalezfx <dvdramos16@gmail.com>
2020-03-11 13:24:49 -0500

    [Feature #1] Calculator functions

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,51 @@
+#include "function_pointers.h"
+/**
+ * op_add - add a with b
+ * @a: int
+ * @b: int
+ * Return: void
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * op_sub - substract a with b
+ * @a: int
+ * @b: int
+ * Return: void
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * op_mult - multiplies a with b
+ * @a: int
+ * @b: int
+ * Return: void
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * op_div - divide a with b
+ * @a: int
+ * @b: int
+ * Return: void
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * op_mod - a module b
+ * @a: int
+ * @b: int
+ * Return: void
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}

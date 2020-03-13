bb3d3d210dc3f02438585fccba00365ddaed4d72
davidgonzalezfx <dvdramos16@gmail.com>
2020-03-13 11:58:08 -0500

    [Header] Struct created

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -4,6 +4,18 @@
 #include <stdio.h>
 #include <stdarg.h>
 
+/**
+ * f - struct for format print
+ * @fmt: char *
+ * @f: function to be called
+ * Return: void
+ */
+struct f
+{
+	char *fmt;
+	int (*f)(char *);
+};
+
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);

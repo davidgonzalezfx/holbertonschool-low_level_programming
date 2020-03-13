2f4e7a49320c38128652c7979dd4077d687d0468
davidgonzalezfx <dvdramos16@gmail.com>
2020-03-13 08:32:02 -0500

    [Header] stdarg.h added

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -2,7 +2,7 @@
 #define VARIADIC_FUNCTIONS_H
 
 #include <stdio.h>
-#include <stdlib.h>
+#include <stdarg.h>
 
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);

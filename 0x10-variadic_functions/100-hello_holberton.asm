95a7c2a1b841180ae3865d4478af15e3455d9563
davidgonzalezfx <dvdramos16@gmail.com>
2020-03-14 13:00:19 -0500

    [Fix] mainmain modified

--- 0x10-variadic_functions/100-hello_holberton.asm
+++ 0x10-variadic_functions/100-hello_holberton.asm
@@ -1,9 +1,8 @@
 section .data
 	msg db	"Hello, Holberton", 10
 
-
 	section .text
-	global mainmain
+	global main
 main:
 	mov     rax, 1
 	mov     rdi, 1
@@ -11,5 +10,5 @@ main:
 	mov     rdx, 17
 	syscall
 	mov    rax, 60
-	mov    rdi, 0	
+	mov    rdi, 0
 	syscall

dba3d0373daea74b8d80dc904b7641a6b31ccb5b
davidgonzalezfx <dvdramos16@gmail.com>
2020-03-19 10:27:38 -0500

    [Feature #1] Adds a new node at the beginning of a list_t list

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,4 +1,5 @@
 #include "lists.h"
+#include <string.h>
 
 /**
  * add_node -
@@ -7,5 +8,15 @@
  */
 list_t *add_node(list_t **head, const char *str)
 {
+	list_t *newNode;
 
+	newNode = malloc(sizeof(list_t));
+	if (!newNode)
+		return (NULL);
+	newNode->str = strdup(str);
+	newNode->len = strlen(str);
+	newNode->next = *head;
+	*head = newNode;
+
+	return (*head);
 }

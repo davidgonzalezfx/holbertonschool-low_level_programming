9e13a0746e4ababdb68ed9311687a835b7b38909
davidgonzalezfx <dvdramos16@gmail.com>
2020-03-19 09:46:55 -0500

    [Feature #1] Count elements of linked list

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,11 +1,16 @@
 #include "lists.h"
 
 /**
- * list_len -
- *
- * Return: 
+ * list_len - return num of element
+ * @h: linked list
+ * Return: num of elemets
  */
 size_t list_len(const list_t *h)
 {
+	int el = 0;
 
+	for (; h; el++)
+		h = h->next;
+
+	return (el);
 }

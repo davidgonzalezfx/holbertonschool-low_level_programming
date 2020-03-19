7bb31a9460076b254c0e2462137b056a536ce668
davidgonzalezfx <dvdramos16@gmail.com>
2020-03-19 11:10:38 -0500

    [Feature #1] Frees a list_t list

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -1,11 +1,18 @@
 #include "lists.h"
 
 /**
- * free_list -
- *
- * Return: 
+ * free_list - free memory allocated for list
+ * @head: start of the list;
+ * Return: void
  */
 void free_list(list_t *head)
 {
+	list_t *actual;
 
+	for (actual = head; actual; actual = head)
+	{
+		head = head->next;
+		free(actual->str);
+		free(actual);
+	}
 }

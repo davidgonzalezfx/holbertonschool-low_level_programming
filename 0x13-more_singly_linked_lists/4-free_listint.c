35622b9a82e1349f6021022048750c5e31e12eb5
davidgonzalezfx <dvdramos16@gmail.com>
2020-03-23 09:30:47 -0500

    [Feature #1] Frees a listint_t list

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -1,11 +1,17 @@
 #include "lists.h"
 
 /**
- * free_listint -
- *
- * Return: 
+ * free_listint - frees a listint_t list
+ * @head: list to be free
+ * Return: void
  */
 void free_listint(listint_t *head)
 {
+	listint_t *actual;
 
+	for (actual = head; actual; actual = head)
+	{
+		head = head->next;
+		free(actual);
+	}
 }

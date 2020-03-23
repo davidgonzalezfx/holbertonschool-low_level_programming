a30392de8401f74ff081a96f0db915286587c209
davidgonzalezfx <dvdramos16@gmail.com>
2020-03-23 09:47:58 -0500

    [Feature #1] Frees a listint_t list and set *head to NULL

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -1,11 +1,19 @@
 #include "lists.h"
 
 /**
- * free_listint2 -
- *
- * Return: 
+ * free_listint2 - frees a listint_t list ans set head to NULL
+ * @head: listint_t
+ * Return: void
  */
 void free_listint2(listint_t **head)
 {
+	listint_t *actual = *head;
 
+	while ((actual = *head) != NULL)
+	{
+		*head = actual->next;
+		free(actual);
+	}
+
+	*head = NULL;
 }

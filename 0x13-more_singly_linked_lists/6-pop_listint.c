bbfe8219f186ea76a2898ee8b1d849312d595631
davidgonzalezfx <dvdramos16@gmail.com>
2020-03-23 09:55:12 -0500

    [Feature #1] Deletes the head node of a listint_t linked list and returns the head node’s data (n)

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -1,11 +1,17 @@
 #include "lists.h"
 
 /**
- * pop_listint -
- *
- * Return: 
+ * pop_listint - Deletes the head node of a listint_t linked list
+ * @head: listint_t list
+ * Return: the head node’s data (n).
  */
 int pop_listint(listint_t **head)
 {
+	listint_t *actual = *head;
+	int num = actual->n;
 
+	*head = actual->next;
+	free(actual);
+
+	return (num);
 }

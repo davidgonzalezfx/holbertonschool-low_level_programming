76bd3ba1a28cd75cf499654d23f60faca80e5c1e
davidgonzalezfx <dvdramos16@gmail.com>
2020-03-23 10:01:30 -0500

    [Fix #1] Betty linter

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -1,11 +1,18 @@
 #include "lists.h"
 
 /**
- * get_nodeint_at_index -
- *
- * Return: 
+ * get_nodeint_at_index - Returns the nth node of a listint_t linked list
+ * @head: listint_t
+ * @index: unsigned int
+ * Return: adress of nth node
  */
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
+	listint_t *indexNode = head;
+	unsigned int actualPos = 0;
 
+	for (actualPos = 0; actualPos < index; actualPos++)
+		indexNode = indexNode->next;
+
+	return (indexNode);
 }

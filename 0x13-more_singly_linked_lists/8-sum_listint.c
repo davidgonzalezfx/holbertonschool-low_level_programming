06c54927361d77777ac004cd59ac45028d637790
davidgonzalezfx <dvdramos16@gmail.com>
2020-03-23 10:08:55 -0500

    [Feature #1] Add all the data (n) of a listint_t linked list

--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -1,11 +1,17 @@
 #include "lists.h"
 
 /**
- * sum_listint -
- *
- * Return: 
+ * sum_listint - Add all the data (n) of a listint_t linked list
+ * @head: listint_t
+ * Return: int sum of all nodes
  */
 int sum_listint(listint_t *head)
 {
+	listint_t *actual = head;
+	int sum = 0;
 
+	for (; actual; actual = actual->next)
+		sum += actual->n;
+
+	return (sum);
 }

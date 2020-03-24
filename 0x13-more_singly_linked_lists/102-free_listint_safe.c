c7d622f4f219fe3c6c4669ca02276ecebc9779e6
davidgonzalezfx <dvdramos16@gmail.com>
2020-03-24 14:49:46 -0500

    [Fix #2] Check for a valid list

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -1,9 +1,9 @@
 #include "lists.h"
 
 /**
- * free_listint_safe -
- *
- * Return: 
+ * free_listint_safe - free memory (save mode)
+ *	@h: list
+ * Return: length of list h
  */
 size_t free_listint_safe(listint_t **h)
 {
@@ -11,7 +11,7 @@ size_t free_listint_safe(listint_t **h)
 	unsigned int cnt = 0;
 
 	forward = *h;
-	if (forward == NULL)
+	if (forward == NULL || h == NULL)
 		return (0);
 
 	while (forward != NULL)

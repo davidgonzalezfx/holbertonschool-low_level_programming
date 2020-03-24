e56282ae8b597b4571b2d10c57c0b0c6e5b3f0f8
davidgonzalezfx <dvdramos16@gmail.com>
2020-03-24 10:34:00 -0500

    [Fix #2] Validation *head unneeded

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -32,7 +32,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 
 		return (1);
 	}
-	else if (index == 0 && *head)
+	else if (index == 0)
 	{
 		nxt = nxt->next;
 		free(*head);

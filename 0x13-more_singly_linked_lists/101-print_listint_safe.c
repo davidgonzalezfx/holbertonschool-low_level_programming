ede26210afc845a361fdbeb9a25bfc31537509c9
davidgonzalezfx <dvdramos16@gmail.com>
2020-03-24 14:06:56 -0500

    [Feature #1] detects infinite loop

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,11 +1,32 @@
 #include "lists.h"
 
 /**
- * print_listint_safe -
- *
- * Return: 
+ * print_listint_safe - detects infinite loop
+ * @head: linked list
+ * Return: length of list
  */
 size_t print_listint_safe(const listint_t *head)
 {
+	const listint_t *forward, *back;
+	unsigned int cnt = 0;
 
+	forward = head;
+	if (forward == NULL)
+		return (0);
+
+	while (forward != NULL)
+	{
+		back = forward;
+		forward = forward->next;
+
+		printf("[%p] %i\n", (void *)back, back->n);
+		cnt++;
+		if (forward >= back)
+		{
+			printf("-> [%p] %i\n", (void *)forward, forward->n);
+			break;
+		}
+	}
+
+	return (cnt);
 }

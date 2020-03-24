26fdf4f9ef495bab672ce765252a6c1594233436
davidgonzalezfx <dvdramos16@gmail.com>
2020-03-24 09:59:35 -0500

    [Fix #1] Refactor

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -9,20 +9,42 @@
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	listint_t *newNode = malloc(sizeof(listint_t));
+	listint_t *newNode;;
 	listint_t *actual = *head;
 	unsigned int pos = 0;
 
+	newNode = malloc(sizeof(listint_t));
+	if (!newNode)
+		return (NULL);
+
 	newNode->n = n;
+	newNode->next = NULL;
 
-	for (; pos < idx; pos++, actual = actual->next)
-	;
-	newNode->next = actual;
+	if (*head == NULL && idx > 0)
+	{
+		free(newNode);
+		return (NULL);
+	}
 
-	actual = *head;
-	for (pos = 0; pos < idx - 1; pos++, actual = actual->next)
-	;
+	if (idx == 0)
+	{
+		newNode->next = *head;
+		*head = newNode;
+		return (newNode);
+	}
+
+	for (; pos < idx - 1; pos++)
+	{
+		actual = actual->next;
+		if (actual == NULL && idx - pos > 0)
+		{
+			free(newNode);
+			return (NULL);
+		}
+	}
+
+	newNode->next = actual->next;
 	actual->next = newNode;
 
-	return (*head);
+	return (newNode);
 }

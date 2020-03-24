27edc05ab0533795e16df8aaaf9a6462c178747f
davidgonzalezfx <dvdramos16@gmail.com>
2020-03-24 14:57:39 -0500

    [Fix #1] Detects infinite loop

--- 0x13-more_singly_linked_lists/103-find_loop.c
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -1,11 +1,33 @@
 #include "lists.h"
 
 /**
- * find_listint_loop -
- *
- * Return: 
+ * find_listint_loop - detects infinite loop
+ * @head: linked list *
+ * Return: Addres where loop starts
  */
 listint_t *find_listint_loop(listint_t *head)
 {
+	listint_t *forwd = head, *back;
 
+	if (forwd == NULL)
+		return (0);
+
+	if (forwd == forwd->next)
+		return (forwd);
+
+	for (; forwd != NULL; )
+	{
+		back = head;
+		forwd = forwd->next;
+
+		for (; back != forwd && forwd != NULL; back = back->next)
+		{
+			if (forwd->next == forwd)
+				return (forwd);
+
+			if (forwd->next == back)
+				return (back);
+		}
+	}
+	return (forwd);
 }

9241ddc9b2949ca1e3db55c4da403dbbbb11f15d
davidgonzalezfx <dvdramos16@gmail.com>
2020-03-24 14:11:09 -0500

    [Feature #1] Free memory (save mode)

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -7,5 +7,23 @@
  */
 size_t free_listint_safe(listint_t **h)
 {
+	const listint_t *forward, *back;
+	unsigned int cnt = 0;
 
+	forward = *h;
+	if (forward == NULL)
+		return (0);
+
+	while (forward != NULL)
+	{
+		back = forward;
+		forward = forward->next;
+
+		free(back);
+		cnt++;
+		if (forward >= back)
+			break;
+	}
+
+	return (cnt);
 }

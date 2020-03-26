012d315b1cc7747e8d1a42cc3831b95e35404dd0
davidgonzalezfx <dvdramos16@gmail.com>
2020-03-26 10:10:30 -0500

    [Feature #1] Converts bin to ui decimal

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,11 +1,39 @@
 #include "holberton.h"
 
 /**
- * binary_to_uint -
- *
- * Return: 
+ * _strlen - return length of string
+ * @s: char
+ * Return: int
+ */
+int _strlen(char *s)
+{
+	int cnt;
+
+	for (cnt = 0; *(s + cnt) != '\0'; cnt++)
+	{
+	}
+	return (cnt);
+}
+/**
+ * binary_to_uint - converts binary to ui decimal
+ * @b: string binary
+ * Return: unsigned int - decimal
  */
 unsigned int binary_to_uint(const char *b)
 {
+	unsigned int dec = 0;
+	int size = 0, i = 1;
 
+	if (!b)
+		return (0);
+
+	size = _strlen((char *)b);
+	for (size--; size >= 0; size--, i *= 2)
+	{
+		if (b[size] != '0' && b[size] != '1')
+			return (0);
+		dec += (b[size] - 48) * i;
+	}
+
+	return (dec);
 }

5e54083c42fae388c878a7f7a1869192b8244e9e
davidgonzalezfx <dvdramos16@gmail.com>
2020-03-26 10:32:16 -0500

    [Feature #1] Prints dec in bin with bitwise operator

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,11 +1,29 @@
 #include "holberton.h"
-
 /**
- * print_binary -
- *
- * Return: 
+ * dec_bin - print in binary with bitwise operator
+ * @n: decimal number
+ * Return: void - just prints
+ */
+void dec_bin(unsigned long int n)
+{
+	if (n == 0)
+		return;
+	else
+	{
+		dec_bin(n >> 1);
+		n = n & 1;
+		_putchar(n + 48);
+	}
+}
+/**
+ * print_binary - print decimal in binary
+ * @n: decimal number
+ * Return: void - just prints
  */
 void print_binary(unsigned long int n)
 {
-
+	if (n == 0)
+		_putchar('0');
+	else
+		dec_bin(n);
 }

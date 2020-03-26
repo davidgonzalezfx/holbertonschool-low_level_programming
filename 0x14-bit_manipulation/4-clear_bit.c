6734926a9c9639a8fc588f05e423ee82d2f2a538
davidgonzalezfx <dvdramos16@gmail.com>
2020-03-26 11:02:17 -0500

    [Feature #1] Set to 0 the bit at a given index of n num

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -1,11 +1,20 @@
 #include "holberton.h"
 
 /**
- * clear_bit -
- *
- * Return: 
+ * clear_bit - set index bit of n to 0
+ * @n: decimal num
+ * @index: index to change bit to 0
+ * Return: 1 if it worked, or -1 if an error occurred
  */
 int clear_bit(unsigned long int *n, unsigned int index)
 {
+	unsigned long int changed;
 
+	if (index > 63)
+		return (-1);
+
+	changed = 1 << index;
+	*n = *n & ~changed;
+
+	return (1);
 }

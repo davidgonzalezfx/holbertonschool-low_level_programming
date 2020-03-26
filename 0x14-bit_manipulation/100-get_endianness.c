3abaf347bc9ff06547af7feab951000c77f7fce3
davidgonzalezfx <dvdramos16@gmail.com>
2020-03-26 12:22:20 -0500

    [Feature #1] Check little and big endian

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -1,11 +1,15 @@
 #include "holberton.h"
 
 /**
- * get_endianness -
- *
- * Return: 
+ * get_endianness - checks the endianness.
+ * Return: int: 0 if big, 1 if little
  */
 int get_endianness(void)
 {
+	int x = 1;
+	char *c = (char *)&x;
 
+	if (*c)
+		return (1);
+	return (0);
 }

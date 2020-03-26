d822ecfdd0eb47eed9997bb1cecc17af0d882038
davidgonzalezfx <dvdramos16@gmail.com>
2020-03-26 10:54:45 -0500

    [Feature #1] Set to 1 the bit at a given index of n num

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -1,11 +1,20 @@
 #include "holberton.h"
 
 /**
- * set_bit -
- *
- * Return: 
+ * set_bit - set index bit of n to 1
+ * @n: decimal num
+ * @index: index to change bit to 1
+ * Return: 1 if it worked, or -1 if an error occurred
  */
 int set_bit(unsigned long int *n, unsigned int index)
 {
+	unsigned long int changed;
 
+	if (index > 63)
+		return (-1);
+
+	changed = 1 << index;
+	*n = *n | changed;
+
+	return (1);
 }

d8fec1b2c930da5ced15c626e7c40afecfe33828
davidgonzalezfx <dvdramos16@gmail.com>
2020-03-26 10:47:26 -0500

    [Feature #1] Get a bit at a given index

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,11 +1,15 @@
 #include "holberton.h"
 
 /**
- * get_bit -
- *
- * Return: 
+ * get_bit - return the bit of n at index
+ * @n: unsigned long int - decimal num
+ * @index: unsigned int - index
+ * Return: int
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
+	if (index > 63)
+		return (-1);
 
+	return ((n >> index) & 1);
 }

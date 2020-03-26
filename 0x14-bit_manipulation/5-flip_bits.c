6240793aca9cbdaf89afdbebfd50a8f6711f9158
davidgonzalezfx <dvdramos16@gmail.com>
2020-03-26 11:14:00 -0500

    [Feature #1] Return how many bits have to change to go from num1 to num2

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -1,11 +1,23 @@
 #include "holberton.h"
 
 /**
- * flip_bits -
- *
- * Return: 
+ * flip_bits - returns the number of bits you would
+ * need to flip to get from one number to another.
+ * @n: num1 - decimal
+ * @m: num2 - decimal
+ * Return: number of bits that have to change
  */
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
+	unsigned long int num_bits_bin = 0;
+	int bits_to_change = 0;
 
+	num_bits_bin = n ^ m;
+	for (; num_bits_bin; )
+	{
+		bits_to_change += num_bits_bin & 1;
+		num_bits_bin >>= 1;
+	}
+
+	return (bits_to_change);
 }

3b07ed85af42b638399e626b645236727a1d28ba
davidgonzalezfx <dvdramos16@gmail.com>
2020-03-31 15:01:35 -0500

    [Fix] Betty linter

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -21,7 +21,7 @@ int create_file(const char *filename, char *text_content)
 	{
 		for (; *(text_content + size); size++)
 		{};
-	
+
 		writed = write(filedes, text_content, size);
 		if (writed == error)
 			return (-1);

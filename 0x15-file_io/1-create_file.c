6ce13946a11bd57dd0a7e5e864a47b0795c113d4
davidgonzalezfx <dvdramos16@gmail.com>
2020-03-31 14:56:18 -0500

    [Feature #1] Creating a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,11 +1,32 @@
 #include "holberton.h"
 
 /**
- * create_file -
- *
- * Return: 
+ * create_file - function that creates a file
+ * @filename: pathname to the file
+ * @text_content: content to write in the nre file
+ * Return: Return 1 on succes, -1 if fails
  */
 int create_file(const char *filename, char *text_content)
 {
+	int filedes, writed, size, error = -1;
 
+	for (; *(text_content + size); size++)
+	{}
+
+	if (!filename)
+		return (-1);
+
+	filedes = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	if (filedes == error)
+		return (-1);
+
+	if (text_content)
+	{
+		writed = write(filedes, text_content, size);
+		if (writed == error)
+			return (-1);
+	}
+
+	close(filedes);
+	return (1);
 }

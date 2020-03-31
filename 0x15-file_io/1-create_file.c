f629fdfd060a97f8aac6145f8e8478997567e76f
davidgonzalezfx <dvdramos16@gmail.com>
2020-03-31 15:00:53 -0500

    [Fix #1] size initialized, get length before write)

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -8,10 +8,7 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int filedes, writed, size, error = -1;
-
-	for (; *(text_content + size); size++)
-	{}
+	int filedes, writed, size = 0, error = -1;
 
 	if (!filename)
 		return (-1);
@@ -22,6 +19,9 @@ int create_file(const char *filename, char *text_content)
 
 	if (text_content)
 	{
+		for (; *(text_content + size); size++)
+		{};
+	
 		writed = write(filedes, text_content, size);
 		if (writed == error)
 			return (-1);

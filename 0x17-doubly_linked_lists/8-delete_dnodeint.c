ea45ba51e3fc24f5952a248feb486c5856875063
davidgonzalezfx <dvdramos16@gmail.com>
2020-05-03 15:13:54 -0500

    Feature - deletes the node at index index

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -1,11 +1,38 @@
 #include "lists.h"
 
 /**
- * delete_dnodeint_at_index -
- *
- * Return: 
+ * delete_dnodeint_at_index - deletes the node at index index
+ * @head: doubly linkedlist
+ * @index: index to remove node
+ * Return: 1 if removed, -1 if fails
  */
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
+	dlistint_t *actual = *head;
+	unsigned int cnt;
 
+	if (!head || !actual)
+		return (-1);
+
+	if (index == 0)
+	{
+		actual = actual->next;
+		free(*head);
+		*head = actual;
+		return (1);
+	}
+
+	for (cnt = 0; actual; cnt++)
+	{
+		if (cnt == index)
+		{
+			(actual->prev)->next = actual->next;
+			(actual->next)->prev = actual->prev;
+			free(actual);
+			return (1);
+		}
+		actual = actual->next;
+	}
+
+	return (-1);
 }

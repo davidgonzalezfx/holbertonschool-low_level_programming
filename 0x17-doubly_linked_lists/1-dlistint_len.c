079c4253768cbde90d5371913b0f6ac1d66c0ba3
davidgonzalezfx <dvdramos16@gmail.com>
2020-05-02 20:15:09 -0500

    Fix - Betty style linter

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -2,14 +2,14 @@
 
 /**
  * dlistint_len - return length of linkedlist
- * h: doubly linkedlist
+ * @h: doubly linkedlist
  * Return: number of elements in a linked dlistint_t list
  */
 size_t dlistint_len(const dlistint_t *h)
 {
 	size_t length = 0;
 
-	while(h)
+	while (h)
 		h = h->next, length++;
 
 	return (length);

5202d1d3606cbd7e16e6273596bd2bb46a0315a5
davidgonzalezfx <dvdramos16@gmail.com>
2020-05-02 23:19:56 -0500

    Fix - Betty style linter

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -8,31 +8,31 @@
  */
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
-    dlistint_t *new_node = malloc(sizeof(dlistint_t));
-    dlistint_t *actual;
+	dlistint_t *new_node = malloc(sizeof(dlistint_t));
+	dlistint_t *actual;
 
-    if (!new_node || !head)
-        return (NULL);
+	if (!new_node || !head)
+		return (NULL);
 
-    new_node->n = n;
+	new_node->n = n;
 
-    if (!(*head))
-    {
-        *head = new_node;
-        new_node->prev = NULL;
-    }
-    else
-    {
-        actual = *head;
+	if (!(*head))
+	{
+		*head = new_node;
+		new_node->prev = NULL;
+	}
+	else
+	{
+		actual = *head;
 
-        while(actual->next)
-            actual = actual->next;
+		while (actual->next)
+			actual = actual->next;
 
-        new_node->next = NULL;
-        new_node->prev = actual;
-        actual->next = new_node;
-    }
-    
-    return (new_node);
+		new_node->next = NULL;
+		new_node->prev = actual;
+		actual->next = new_node;
+	}
+
+	return (new_node);
 
 }

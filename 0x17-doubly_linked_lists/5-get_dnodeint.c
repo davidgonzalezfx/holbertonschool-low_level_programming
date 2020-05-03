1fc1c25a0d4c184d00d85323b7c7c56a66500ecb
davidgonzalezfx <dvdramos16@gmail.com>
2020-05-02 23:36:18 -0500

    Feature - gets the nth node of a dlistint_t linkedlist

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -1,11 +1,22 @@
+
 #include "lists.h"
 
 /**
- * get_dnodeint_at_index -
- *
- * Return: 
+ * get_dnodeint_at_index - gets nth node
+ * @head: doubly linkedlist
+ * @index: nth node to be access
+ * Return: the nth node of a dlistint_t linked list
  */
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
+	dlistint_t *actual = head;
+	int nth = 0;
 
+	if (!head)
+		return (NULL);
+
+	while (nth++ != (int)index)
+		actual = actual->next;
+
+	return (actual);
 }

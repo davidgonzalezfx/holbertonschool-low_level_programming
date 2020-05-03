99066830ba46ed358035494f7c1f1f4ed6b34a17
davidgonzalezfx <dvdramos16@gmail.com>
2020-05-02 23:55:09 -0500

    Feature - sum of all nodes of doubly linkedlist

--- 0x17-doubly_linked_lists/6-sum_dlistint.c
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -1,11 +1,22 @@
 #include "lists.h"
 
 /**
- * sum_dlistint -
- *
- * Return: 
+ * sum_dlistint - sum of all nodes of doubly linkedlist
+ * @head: doubly linkedlist
+ * Return: return the sum
  */
 int sum_dlistint(dlistint_t *head)
 {
+	if (!head)
+		return (0);
 
+	int acumm = 0;
+
+	while (head)
+	{
+		acumm += head->n;
+		head = head->next;
+	}
+
+	return (acumm);
 }

72f7db64f63d280665ac9bb8cbc78f2e17033780
davidgonzalezfx <dvdramos16@gmail.com>
2020-05-28 20:26:44 -0500

    Feature - deletes a hash table

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -1,11 +1,31 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_delete -
- *
- * Return: 
+ * hash_table_delete - deletes a hash table
+ * @ht: is the hastable
+ * Return: void, just free everything
  */
 void hash_table_delete(hash_table_t *ht)
 {
+	unsigned long int idx;
+	hash_node_t *actual, *nxt;
 
+	if (!ht)
+		return;
+
+	for (idx = 0; idx < ht->size; idx++)
+	{
+		actual = ht->array[idx];
+		while (actual)
+		{
+			nxt = actual->next;
+			free(actual->key);
+			free(actual->value);
+			free(actual);
+			actual = nxt;
+		}
+	}
+
+	free(ht->array);
+	free(ht);
 }

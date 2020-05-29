c0f3496738d67d041d0e28d0c2b2fd53d71cdfc8
davidgonzalezfx <dvdramos16@gmail.com>
2020-05-28 19:16:16 -0500

    Feature - retrieves a value associated with a key

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -1,11 +1,29 @@
 #include "hash_tables.h"
+#include <string.h>
 
 /**
- * hash_table_get -
- *
- * Return: 
+ * hash_table_get - retrieves a value associated with a key
+ * @ht: is the hash table to look into
+ * @key: is the key looking for
+ * Return: Returns the value associated with the element
  */
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
+	unsigned int pos;
+	hash_node_t *actual;
 
+	if (!ht || !key || !(*key))
+		return (NULL);
+
+	pos = key_index((unsigned char *)key, ht->size);
+	actual = ht->array[pos];
+
+	while (actual)
+	{
+		if (!strcmp(key, actual->key))
+			return (actual->value);
+		actual = actual->next;
+	}
+
+	return (NULL);
 }

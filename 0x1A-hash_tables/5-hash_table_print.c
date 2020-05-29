fa17e5fcc39ee8044fa534e68e4ebd65cc35a502
davidgonzalezfx <dvdramos16@gmail.com>
2020-05-28 19:39:43 -0500

    Feature - prints a hash table

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -1,11 +1,29 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_print -
- *
- * Return: 
+ * hash_table_print - prints a hash table
+ * @ht: is the hashtable
+ * Return: void, just print
  */
 void hash_table_print(const hash_table_t *ht)
 {
+	unsigned long int idx, n_el = 0;
+	hash_node_t *actual;
 
+	if (!ht)
+		return;
+
+	printf("{");
+	for (idx = 0; idx < ht->size; idx++)
+	{
+		actual = ht->array[idx];
+		while (actual)
+		{
+			if (n_el++ > 0)
+				printf(", ");
+			printf("'%s': '%s'", actual->key, actual->value);
+			actual = actual->next;
+		}
+	}
+	printf("}\n");
 }
